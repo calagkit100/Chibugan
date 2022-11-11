cc6380dd59b2abe98d0ed95652ae9a0cf06c477d
calagkit100 <calagkit100@gmail.com>
2022-11-11 18:56:05 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//variables
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
