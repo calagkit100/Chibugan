e849e9b9ee775421ab00ef482ecc7c1025de3ee1
calagkit100 <calagkit100@gmail.com>
2022-11-17 20:23:32 +0800

    done upto footer

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 //variables
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector(".navbar");
 
@@ -34,4 +65,66 @@ var swiper = new Swiper(".home-slider", {
         clickable: true,
     },
     loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 140,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidePerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+//SCROLL BEHAVIOUR
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+//PLUS MINUS BUTTONS CART 
+$('.add').click(function() {
+    $(this).prev().val(+$(this).prev().val() + 1);
+});
+$('.sub').click(function() {
+    if ($(this).next().val() > 0) $(this).next().val(+$(this).next().val() - 1);
 });
