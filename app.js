438bf599654d163aed936df7c36b7b95fa26f989
calagkit100 <calagkit100@gmail.com>
2022-11-17 12:40:55 +0800

    dishes-about added

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ menu.onclick = () => {
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 }
+
+
+menu.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle("active");
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove("active");
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 140,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
